86e763e9468f6cff16191c53c9e4ad9386a8834d
Melik Fırat Başçi <70327175+MelikFiratBasci@users.noreply.github.com>
2024-01-05 16:58:39 +0300

    Create laplace.py

---
+++ laplace.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image_url = "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
+image_response = urlopen(image_url)
+image_array = np.asarray(bytearray(image_response.read()), dtype=np.uint8)
+image = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE)
+
+laplacian_filtered_image = cv2.Laplacian(image, cv2.CV_64F)
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1), plt.imshow(image, cmap='gray'), plt.title('Original Image')
+plt.subplot(1, 2, 2), plt.imshow(np.abs(laplacian_filtered_image), cmap='gray'), plt.title('Laplacian Filtered Image')
+plt.show()
